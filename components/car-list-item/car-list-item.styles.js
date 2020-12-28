bf75a6be55530f374f92798346b656ccb7f6cf7e
Volodymyr Vasylkiv <vvas1@softserveinc.com>
2020-12-28 17:50:42 +0200

    added new car-list-item

--- components/car-list-item/car-list-item.styles.js
+++ components/car-list-item/car-list-item.styles.js
@@ -4,42 +4,68 @@ export const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  paper: {
-    padding: theme.spacing(2),
-    margin: "auto",
-    height: "100%",
-    display: "grid",
-    alignItems: "stretch",
-    minHeight: "10rem",
+  mainContainer: {
+    height: "12vw",
 
+    "@media (max-width:2560px)": {
+      height: "9vw",
+    },
+    "@media (max-width:1980px)": {
+      height: "12vw",
+    },
+    "@media (max-width:1440px)": {
+      height: "16vw",
+    },
+    "@media (max-width:1024px)": {
+      height: "auto",
+    },
   },
-  image: (props) => ({
-    alignSelf: "stretch",
-    background: `url(${props.photo}) no-repeat center`,
-    backgroundSize: "cover",
-    minHeight: "10vh",
+  imageContainer: {
+    alignSelf: "center",
+    justifyContent: "center",
+    height: "100%",
     "@media (max-width:959px)": {
-      height: "25vw",
+      height: "30vw",
+    },
+    "@media (max-width:500px)": {
+      height: "100%",
+    },
+  },
+  image: {
+    height: "100%",
+    width: "100%",
+    padding: "8px 0 8px 8px",
+    "@media (max-width:959px)": {
+      height: "30vw",
+      padding: 0,
     },
     "@media (max-width:500px)": {
       height: "60vw",
     },
-  }),
-  textDiv: {
-    padding: "0 0 0 8px !important",
+  },
+  title: {
+    fontSize: "1.1rem",
+  },
+  textContainer: {
+    height: "100%",
+    padding: "8px",
     "@media (max-width:959px)": {
-      padding: "8px 0 !important",
+      height: "auto",
     },
   },
-  buttonDiv: {
-    padding: "8px !important",
-    "& button": {
-      padding: "0.5rem",
-      backgroundColor: "darkblue",
-      color: "white",
+  titleDiv: {
+    height: "50%",
+    "@media (max-width:959px)": {
+      height: "10vw",
     },
-    "& button:hover": {
-      backgroundColor: "blue",
-    },
+  },
+
+  price: {
+    color: "green",
+    fontWeight: 700,
+  },
+  buttonContainer: {
+    alignSelf: "flex-end",
+    justifyContent: "flex-end",
   },
 }));
